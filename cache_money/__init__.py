1962b0e8bd75b192af22dcd4405128682060484c
Alexandre Jutras <alexandre.jutras@elementai.com>
2021-11-02 22:36:39 -0400

    linting

--- cache_money/__init__.py
+++ cache_money/__init__.py
@@ -1,4 +1,4 @@
-from cache_money.cache import cache_money, close_cache_money, init_cache_money
 from cache_money import utils as cache_utils
+from cache_money.cache import cache_money, close_cache_money, init_cache_money
 
 __all__ = ["cache_money", "close_cache_money", "init_cache_money", "cache_utils"]
