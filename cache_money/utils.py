58f4d8f94e7ff0d273406fe6a48c5a40b570b662
Alexandre Jutras <alexandre.jutras@elementai.com>
2021-11-02 22:24:23 -0400

    Adding try/except around pickle.loads, adding an option to bypass key transformation on get/set operation

--- cache_money/utils.py
+++ cache_money/utils.py
@@ -1,2 +1,10 @@
+import inspect
+from typing import Callable
+
+
 def decode(s):
     return s.decode("utf-8") if isinstance(s, bytes) else s
+
+
+def get_function_path(fn: Callable) -> str:
+    return f"{inspect.getmodule(fn).__name__}.{fn.__name__}"
